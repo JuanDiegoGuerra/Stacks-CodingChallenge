f0126813b718d31454b29f455c6cb1593a47d614
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-29 11:15:29 -0500

    Create challenge.js
    main file for the challenge, you will solve it here.

---
+++ challenge.js
@@ -0,0 +1,32 @@
+class Stack {
+  
+
+  push(number) {
+    // your code here
+    
+  }
+  
+  pop() {
+    // your code here
+        
+  }
+}
+
+const stack = new Stack()
+stack.push(3)
+stack.push(5)
+console.log(stack.pop())
+// => 5
+
+stack.push(2)
+stack.push(7)
+console.log(stack.pop())
+// => 7
+
+console.log(stack.pop())
+// => 2
+
+console.log(stack.pop())
+// => 3
+
+module.exports = Stack
